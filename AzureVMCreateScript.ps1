34e36d385b634841e1b291d78d1885a22a6782d3
Dushyant <dushyantsingh.dsindia@gmail.com>
2020-09-28 22:32:32 -0700

    Intial Commit

---
+++ AzureVMCreateScript.ps1
@@ -0,0 +1,7 @@
+﻿set-variable -name "USERNAME" -value "Your_UserName"
+
+Set-Variable -name "PASSWORD" -value "Password"
+
+New-AzResourceGroup -Name Your_ResourceGroup_Name -Location EastUS2
+
+az vm create --name VM_Name --resource-group Your_ResourceGroup_Name --image 'Win2016Datacenter' --size Standard_B1s --location EastUS2 --admin-username $USERNAME --admin-password $PASSWORD --zone 2
